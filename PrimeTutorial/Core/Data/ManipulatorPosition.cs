017fd9553d51d0ad6739d93b970783930718a326
Ilya Telegin <ilya.a.telegin@gmail.com>
2014-05-25 19:26:56 +0600

    Implement manipulator position data class

---
+++ PrimeTutorial/Core/Data/ManipulatorPosition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PrimeTutorial.Core.Data
+{
+    [Serializable]
+    public class ManipulatorPosition
+    {
+        public double X { get; private set; }
+
+        public double Y { get; private set; }
+
+        public double Alpha { get; private set; }
+
+        public ManipulatorPosition(double x, double y, double alpha)
+        {
+            X = x;
+            Y = y;
+            Alpha = alpha;
+        }
+    }
+}
