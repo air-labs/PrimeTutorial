b7a177709ce14a829ba0d274e3717f97d63f1231
Ilya Telegin <ilya.a.telegin@gmail.com>
2014-05-25 19:45:33 +0600

    Implement manipulator signals data class

---
+++ PrimeTutorial/Core/Data/ManipulatorSignals.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PrimeTutorial.Core.Data
+{
+    [Serializable]
+    public class ManipulatorSignals
+    {
+        public int Shoulder { get; private set; }
+
+        public int Elbow { get; private set; }
+
+        public int Palm { get; private set; }
+
+        public ManipulatorSignals(int shoulder, int elbow, int palm)
+        {
+            Shoulder = shoulder;
+            Elbow = elbow;
+            Palm = palm;
+        }
+    }
+}
