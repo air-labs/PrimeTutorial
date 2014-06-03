d39d5bc02ff6f3664b1bc70f8fa334e394d77233
Ilya Telegin <ilya.a.telegin@gmail.com>
2014-06-04 04:47:38 +0600

    Implement Dwm data class

---
+++ PrimeTutorial/Core/Data/Dwm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PrimeTutorial.Core.Data
+{
+    [Serializable]
+    public class Dwm
+    {
+        public double VLeft0 { get; private set; }
+        public double VRight0 { get; private set; }
+
+        public double VLeft1 { get; private set; }
+        public double VRgiht1 { get; private set; }
+
+        public double Time { get; private set; }
+
+        public Dwm(double vLeft0, double vRight0, double vLeft1, double vRgiht1, double time)
+        {
+            VLeft0 = vLeft0;
+            VRight0 = vRight0;
+            VLeft1 = vLeft1;
+            VRgiht1 = vRgiht1;
+            Time = time;
+        }
+    }
+}
