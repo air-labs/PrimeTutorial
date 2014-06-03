ae5c01c009c5a925664fd6068c38e35c1465a7d4
Ilya Telegin <ilya.a.telegin@gmail.com>
2014-06-03 22:19:44 +0600

    Implement manipulator emulator functional block

---
+++ PrimeTutorial/Core/FunctionalBlocks/ManipulatorEmulator.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Windows.Forms;
+using Prime;
+using PrimeTutorial.Core.Data;
+using PrimeTutorial.Core.Forms;
+
+namespace PrimeTutorial.Core.FunctionalBlocks
+{
+    public class ManipulatorEmulator : IFunctionalBlock<ManipulatorAngles, bool>
+    {
+        private readonly ManipulatorForm _form = new ManipulatorForm();
+
+        public ManipulatorEmulator()
+        {
+            new Thread(() => Application.Run(_form)).Start();
+        }
+
+        public bool Process(ManipulatorAngles input)
+        {
+            _form.SetAngles(input);
+            return true;
+        }
+    }
+}
