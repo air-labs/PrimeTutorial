a9bd98a62e69e550c7680f0e9ed0ff5cbd822db8
Ilya Telegin <ilya.a.telegin@gmail.com>
2014-06-04 04:49:11 +0600

    Implement dw emulator functional block

---
+++ PrimeTutorial/Core/FunctionalBlocks/DwEmulator.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Windows.Forms;
+using Prime;
+using PrimeTutorial.Core.Data;
+using PrimeTutorial.Core.Forms;
+
+namespace PrimeTutorial.Core.FunctionalBlocks
+{
+    class DwEmulator : IFunctionalBlock<Dwm, bool>
+    {
+        private readonly DwForm _form = new DwForm();
+
+        public DwEmulator()
+        {
+            new Thread(() => Application.Run(_form)).Start();
+        }
+
+        public bool Process(Dwm input)
+        {
+            _form.PutDwm(input);
+            return true;
+        }
+    }
+}
