b80e5e0e3cd2ef222b7d018eb9295e7df67e06c3
Ilya Telegin <ilya.a.telegin@gmail.com>
2014-06-09 04:12:27 +0600

    Refactor dw emulator functional block and form

--- PrimeTutorial/Core/Forms/DwForm.cs
+++ PrimeTutorial/Core/Forms/DwForm.cs
@@ -8,7 +8,6 @@ namespace PrimeTutorial.Core.Forms
 {
     public partial class DwForm : BaseForm
     {
-        private readonly ConcurrentQueue<Dwm> _queue = new ConcurrentQueue<Dwm>();
         private readonly ConcurrentQueue<Tuple<double, double, double>> _points = new ConcurrentQueue<Tuple<double, double, double>>();
 
         private Matrix matrix = new Matrix();
@@ -18,9 +17,9 @@ namespace PrimeTutorial.Core.Forms
             InitializeComponent();
         }
 
-        public void PutDwm(Dwm dwm)
+        public void PutDwm(Tuple<double, double, double> point)
         {
-            _queue.Enqueue(dwm);
+            _points.Enqueue(point);
         }
 
         protected override void InitializeGraphics()
@@ -41,7 +40,12 @@ namespace PrimeTutorial.Core.Forms
             DrawLine(AxisPen, 0, 10, 0, -10);
             DrawLine(AxisPen, 10, 0, -10, 0);
 
-            Calculate();
+            Tuple<double, double, double> point;
+            if (!_points.IsEmpty && _points.TryDequeue(out point))
+            {
+                matrix.Rotate((float) point.Item3);
+                matrix.Translate((float) point.Item1, (float) point.Item2);
+            }
 
             PushGraphics();
 
@@ -49,51 +53,6 @@ namespace PrimeTutorial.Core.Forms
             DrawRectangle(Brushes.MediumAquamarine, 0, 0, 2, 1);
             
             PopGraphics();
-        }
-
-        private void Calculate()
-        {
-            Dwm dwm;
-            if (_points.IsEmpty && !_queue.IsEmpty && _queue.TryDequeue(out dwm))
-            {
-                double vl = dwm.VLeft1;
-                double vr = dwm.VRgiht1;
-
-                double b = 1;
-                double time = 0.1;
-
-                double r, v, offset;
-                if (Math.Abs(vl + vr) < 0.0001)
-                {
-                    r = b;
-                    v = Math.Abs(vl) + Math.Abs(vr);
-                    offset = 0;
-                }
-                else
-                {
-                    r = b * (Math.Abs(vl) + Math.Abs(vr)) / (Math.Abs(vl) - Math.Abs(vr));
-                    v = (vl + vr) / 2.0;
-                    offset = v;
-                }
-
-                double w = v / r;
-                var steps = (int)dwm.Time / time;
-                for (var i = 0; i < steps; i++)
-                {
-                    double angle = -w * time;
-                    double y = Math.Cos(angle) * offset * time;
-                    double x = Math.Sin(angle) * offset * time;
-
-                    _points.Enqueue(Tuple.Create(x, y, angle * 180 / Math.PI));
-                }
-            }
-
-            Tuple<double, double, double> point;
-            if (!_points.IsEmpty && _points.TryDequeue(out point))
-            {
-                matrix.Rotate((float)point.Item3);
-                matrix.Translate((float)point.Item1, (float)point.Item2);
-            }
         }
     }
 }
--- PrimeTutorial/Core/FunctionalBlocks/DwEmulator.cs
+++ PrimeTutorial/Core/FunctionalBlocks/DwEmulator.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Windows.Forms;
+using NUnit.Framework.Constraints;
 using Prime;
 using PrimeTutorial.Core.Data;
 using PrimeTutorial.Core.Forms;
@@ -10,6 +12,8 @@ namespace PrimeTutorial.Core.FunctionalBlocks
     {
         private readonly DwForm _form = new DwForm();
 
+        private const int Delay = 100;
+
         public DwEmulator()
         {
             new Thread(() => Application.Run(_form)).Start();
@@ -17,7 +21,38 @@ namespace PrimeTutorial.Core.FunctionalBlocks
 
         public bool Process(Dwm input)
         {
-            _form.PutDwm(input);
+            double vl = input.VLeft1;
+            double vr = input.VRgiht1;
+
+            double b = 1;
+            double time = 0.1;
+
+            double r, v, offset;
+            if (Math.Abs(vl + vr) < 0.0001)
+            {
+                r = b;
+                v = Math.Abs(vl) + Math.Abs(vr);
+                offset = 0;
+            }
+            else
+            {
+                r = b * (Math.Abs(vl) + Math.Abs(vr)) / (Math.Abs(vl) - Math.Abs(vr));
+                v = (vl + vr) / 2.0;
+                offset = v;
+            }
+
+            double w = v / r;
+            var steps = (int) input.Time / time;
+            for (var i = 0; i < steps; i++)
+            {
+                double angle = -w * time;
+                double y = Math.Cos(angle) * offset * time;
+                double x = Math.Sin(angle) * offset * time;
+
+                _form.PutDwm(Tuple.Create(x, y, angle * 180 / Math.PI));
+                Thread.Sleep(Delay);
+            }
+
             return true;
         }
     }
