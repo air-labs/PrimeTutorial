e9d5c10380195c1c50ee3ca3724aeb243a6a0358
Ilya Telegin <ilya.a.telegin@gmail.com>
2014-06-08 21:53:20 +0600

    Add sample for if-then-else block

--- PrimeTutorial/Program.cs
+++ PrimeTutorial/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Prime;
 using PrimeTutorial.Core.Data;
+using PrimeTutorial.Core.Extensions;
 using PrimeTutorial.Core.FunctionalBlocks;
 using System;
 
@@ -10,9 +11,21 @@ namespace PrimeTutorial
         static void Main(string[] args)
         {
             var factory = new LibertyFactory();
-            var block = factory.CreateChain(new Driver()).Link(new ManipulatorEmulator()).ToFunctionalBlock();
+
+            var driverChain = factory.CreateChain(new Driver(false));
+
+            var emulatorChain = factory.CreateChain(new ManipulatorEmulator());
+
+            Func<ManipulatorAngles, bool> condition = angles =>
+                !Double.IsNaN(angles.Betta1) && !Double.IsNaN(angles.Betta2) && !Double.IsNaN(angles.Betta3);
+            var ifChain = factory.IfThenElse(condition, emulatorChain, false);
+
+            var block = driverChain.Link(ifChain).ToFunctionalBlock();
 
             Console.WriteLine(block.Process(new ManipulatorPosition(1.0, 1.0, Math.PI / 4)));
+            Console.WriteLine(block.Process(new ManipulatorPosition(0.0, 3.0, -Math.PI / 2)));
+            Console.WriteLine(block.Process(new ManipulatorPosition(3.0, 3.0, -Math.PI / 4)));
+            Console.WriteLine(block.Process(new ManipulatorPosition(3.0, 0.0, 0)));
         }
     }
 }
