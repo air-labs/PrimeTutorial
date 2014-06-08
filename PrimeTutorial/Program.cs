e122b1229649c7b7d892973dad993edfae764972
Ilya Telegin <ilya.a.telegin@gmail.com>
2014-06-08 23:10:44 +0600

    Add demos executing in main programm

--- PrimeTutorial/Program.cs
+++ PrimeTutorial/Program.cs
@@ -1,31 +1,43 @@
-﻿using Prime;
-using PrimeTutorial.Core.Data;
-using PrimeTutorial.Core.Extensions;
-using PrimeTutorial.Core.FunctionalBlocks;
-using System;
+﻿using System;
+using PrimeTutorial.Demos;
 
 namespace PrimeTutorial
 {
-    static class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            var factory = new LibertyFactory();
+            #region Demonstration #1: linking chains by LibertyPrime
 
-            var driverChain = factory.CreateChain(new Driver(false));
+            Console.WriteLine("Demonstration #1: linking chains by LibertyPrime");
+            Demo01.Run();
+            Console.WriteLine();
 
-            var emulatorChain = factory.CreateChain(new ManipulatorEmulator());
+            #endregion
 
-            Func<ManipulatorAngles, bool> condition = angles =>
-                !Double.IsNaN(angles.Betta1) && !Double.IsNaN(angles.Betta2) && !Double.IsNaN(angles.Betta3);
-            var ifChain = factory.IfThenElse(condition, emulatorChain, false);
+            #region Demonstration #2: manual linking blocks
 
-            var block = driverChain.Link(ifChain).ToFunctionalBlock();
+            Console.WriteLine("Demonstration #2: manual linking blocks");
+            Demo02.Run();
+            Console.WriteLine();
 
-            Console.WriteLine(block.Process(new ManipulatorPosition(1.0, 1.0, Math.PI / 4)));
-            Console.WriteLine(block.Process(new ManipulatorPosition(0.0, 3.0, -Math.PI / 2)));
-            Console.WriteLine(block.Process(new ManipulatorPosition(3.0, 3.0, -Math.PI / 4)));
-            Console.WriteLine(block.Process(new ManipulatorPosition(3.0, 0.0, 0)));
+            #endregion
+
+            #region Demonstration #3: emulator
+
+            Console.WriteLine("Demonstration #3: emulator");
+            Demo03.Run();
+            Console.WriteLine();
+
+            #endregion
+
+            #region Demonstration #4: if-then-else
+
+            Console.WriteLine("Demonstration #4: if-then-else");
+            Demo04.Run();
+            Console.WriteLine();
+
+            #endregion
         }
     }
 }
