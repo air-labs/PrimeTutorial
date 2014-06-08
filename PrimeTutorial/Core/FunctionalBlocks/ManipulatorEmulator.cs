709b4567296b23d56b02693aedb8369e8ae72552
Ilya Telegin <ilya.a.telegin@gmail.com>
2014-06-08 10:03:13 +0600

    Refactor manipulator emulator functional block

--- PrimeTutorial/Core/FunctionalBlocks/ManipulatorEmulator.cs
+++ PrimeTutorial/Core/FunctionalBlocks/ManipulatorEmulator.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Threading;
-using System.Windows.Controls.Primitives;
+﻿using System.Threading;
 using System.Windows.Forms;
 using Prime;
 using PrimeTutorial.Core.Data;
 using PrimeTutorial.Core.Forms;
+using PrimeTutorial.Core.FormsData;
 
 namespace PrimeTutorial.Core.FunctionalBlocks
 {
@@ -13,7 +12,7 @@ namespace PrimeTutorial.Core.FunctionalBlocks
         private readonly ManipulatorForm _form = new ManipulatorForm();
         private ManipulatorAngles _angles = new ManipulatorAngles(1.8, 1.5, 1.4);
 
-        private const double Delta = 0.1;
+        private const double Step = 0.1;
         private const int Delay = 100;
 
         public ManipulatorEmulator()
@@ -24,43 +23,19 @@ namespace PrimeTutorial.Core.FunctionalBlocks
 
         public bool Process(ManipulatorAngles input)
         {
-            var sign1 = Math.Sign(input.Betta1 - _angles.Betta1);
-            var beta1 = _angles.Betta1;
-
-            var sign2 = Math.Sign(input.Betta2 - _angles.Betta2);
-            var beta2 = _angles.Betta2;
-
-            var sign3 = Math.Sign(input.Betta3 - _angles.Betta3);
-            var beta3 = _angles.Betta3;
-
-            while (true)
+            var formAngles = new FormAngles(_angles, input, Step);
+            while (formAngles.IsMove())
             {
-                var isBeta1Move = NextStep(ref beta1, sign1 * Delta, input.Betta1);
-                var isBeta2Move = NextStep(ref beta2, sign2 * Delta, input.Betta2);
-                var isBeta3Move = NextStep(ref beta3, sign3 * Delta, input.Betta3);
-
-                _form.SetAngles(new ManipulatorAngles(beta1, beta2, beta3));
+                formAngles.MakeStep();
+                _form.SetAngles(formAngles.ToManipulatorAngles());
                 Thread.Sleep(Delay);
-
-                if (!isBeta1Move && !isBeta2Move && !isBeta3Move)
-                {
-                    break;
-                }
             }
-            
+
+            _form.SetAngles(input);
+            Thread.Sleep(Delay);
+
+
             _angles = input;
-            return true;
-        }
-
-        private bool NextStep(ref double angle, double delta, double target)
-        {
-            if (Math.Abs(angle - target) <= Math.Abs(delta))
-            {
-                angle = target;
-                return false;
-            }
-
-            angle += delta;
             return true;
         }
     }
