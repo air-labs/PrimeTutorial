c38ed40b4f1e67995ee96121d3b1b5f4f9fa684f
Ilya Telegin <ilya.a.telegin@gmail.com>
2014-06-09 11:24:38 +0600

    Implement geometry data class

---
+++ PrimeTutorial/Core/Data/Geometry.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PrimeTutorial.Core.Data
+{
+    [Serializable]
+    public class Geometry
+    {
+        public GeometryType Type { get; private set; }
+
+        public double Value { get; private set; }
+
+        public static Geometry CreateLineGeometry(double value)
+        {
+            return new Geometry(GeometryType.Line, value);
+        }
+
+        public static Geometry CreateSpotTurnGeometry(double value)
+        {
+            return new Geometry(GeometryType.SpotTurn, value);
+        }
+
+        private Geometry(GeometryType type, double value)
+        {
+            Type = type;
+            Value = value;
+        }
+    }
+}
