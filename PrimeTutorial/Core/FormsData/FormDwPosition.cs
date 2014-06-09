e9debd88626c8d801dc553fdc89e0795a48b8930
Ilya Telegin <ilya.a.telegin@gmail.com>
2014-06-09 08:41:49 +0600

    Implement auxiliary form dw position class

---
+++ PrimeTutorial/Core/FormsData/FormDwPosition.cs
@@ -0,0 +1,17 @@
+﻿namespace PrimeTutorial.Core.FormsData
+{
+    public class FormDwPosition
+    {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+
+        public double Angle { get; private set; }
+
+        public FormDwPosition(double x, double y, double angle)
+        {
+            X = x;
+            Y = y;
+            Angle = angle;
+        }
+    }
+}
